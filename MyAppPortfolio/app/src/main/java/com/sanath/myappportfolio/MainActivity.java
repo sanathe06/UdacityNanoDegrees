6f53991a31e30b530cf4e2ab49e2c590c1e8ae3a
Sanath Nandasiri <sanathe06@gmail.com>
2016-07-07 02:30:08 +0530

    My App Portfolio created

---
+++ MyAppPortfolio/app/src/main/java/com/sanath/myappportfolio/MainActivity.java
@@ -0,0 +1,50 @@
+package com.sanath.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mButtonPopularMovies = (Button) findViewById(R.id.button_popular_movies);
+        mButtonPopularMovies.setOnClickListener(this);
+        Button mButtonStockHawk = (Button) findViewById(R.id.button_stock_hawk);
+        mButtonStockHawk.setOnClickListener(this);
+        Button mButtonBuildItBigger = (Button) findViewById(R.id.button_build_it_bigger);
+        mButtonBuildItBigger.setOnClickListener(this);
+        Button mButtonMakeMaterial = (Button) findViewById(R.id.button_make_you_app_material);
+        mButtonMakeMaterial.setOnClickListener(this);
+        Button mButtonGoUbiquitous = (Button) findViewById(R.id.button_go_ubiquitous);
+        mButtonGoUbiquitous.setOnClickListener(this);
+        Button mButtonCapstone = (Button) findViewById(R.id.button_capstone);
+        mButtonCapstone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        String appName;
+        if (id == R.id.button_popular_movies) {
+            appName = getString(R.string.popular_movies);
+        } else if (id == R.id.button_stock_hawk) {
+            appName = getString(R.string.stock_hawk);
+        } else if (id == R.id.button_build_it_bigger) {
+            appName = getString(R.string.build_it_bigger);
+        } else if (id == R.id.button_make_you_app_material) {
+            appName = getString(R.string.make_your_app_material);
+        } else if (id == R.id.button_go_ubiquitous) {
+            appName = getString(R.string.go_ubiquitous);
+        } else if (id == R.id.button_capstone) {
+            appName = getString(R.string.capstone);
+        } else {
+            return;
+        }
+        Toast.makeText(this, String.format(getString(R.string.msg_format), appName), Toast.LENGTH_LONG).show();
+    }
+}
